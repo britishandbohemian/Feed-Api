1d595a7995e10076665d2f9052d0970630012776
britishandbohemian <48567677+britishandbohemian@users.noreply.github.com>
2025-02-09 11:22:39 +0200

    ww
    xcsq

--- utils/asyncHandler.js
+++
@@ -1,8 +0,0 @@
-// utils/asyncHandler.js
-const asyncHandler = fn => {
-  return (req, res, next) => {
-    Promise.resolve(fn(req, res, next)).catch(next);
-  };
-};
-
-export default asyncHandler;
